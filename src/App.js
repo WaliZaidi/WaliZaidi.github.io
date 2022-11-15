37361f2e16a91332d3004b61f00944a352b17895
Wali Hassan <i202429@nu.edu.pk>
2022-11-15 23:29:21 +0500

    check

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and yay save to reload. Wheeeeeeeeeeeeeeee
+          Edit <code>src/App.js</code> and yay save to reload. WheeeeeeeeeeeeeeeeeEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
         </p>
         <a
           className="App-link"
